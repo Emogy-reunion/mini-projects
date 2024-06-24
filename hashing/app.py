a3ed4bad74682d5eb8c226c7aec0186558f36496
Emogy-reunion <mv7786986@gmail.com>
2024-06-24 05:32:31 +0300

    feat: configure sqlalchemy

--- hashing/app.py
+++ hashing/app.py
@@ -2,9 +2,18 @@
 This module contains implementation of password hashing using Bcrypt
 """
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-
+#initialize the application instance
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'MY-SECRET-KEY'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql:mark:7hhYhn>4@localhost/flaskpractice'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db = SQLAlchemy(app)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
