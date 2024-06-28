f253c2a32ec50595743cf1ae99ef0ea2b2559435
Emogy-reunion <mv7786986@gmail.com>
2024-06-28 21:04:33 +0300

    add: import and initialize SQLAlchemy'

--- protect-routes/app.py
+++ protect-routes/app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 #initialize the flask app
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://mark:7hhYhn>4@localhost/flaskpractice'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
