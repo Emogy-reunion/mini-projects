8a8ca887a6811ae15daf4bd0c9c861b549892d4d
Emogy-reunion <mv7786986@gmail.com>
2024-06-28 21:21:24 +0300

    add: add the user's class

--- protect-routes/app.py
+++ protect-routes/app.py
@@ -9,6 +9,21 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    """
+    representation of the user table
+    the table has the following column: id, firstname, lastname, email, passwordhash
+    """
+    __tablename__ = 'user1'
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    firstname = db.Column(db.String(50), nullable=False)
+    lastname = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    passwordhash = db.Column(db.String(50), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
 
 
 
