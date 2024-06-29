2c992e0ef0c7da0f56b798f7f93c2d7778e0c3b8
Emogy-reunion <mv7786986@gmail.com>
2024-06-29 04:42:17 +0300

    add: add another column on user table

--- protect-routes/app.py
+++ protect-routes/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, InputRequired
+from wtforms.validators import DataRequired, InputRequire d
 
 #initialize the flask app
 app = Flask(__name__)
@@ -16,13 +16,14 @@ db = SQLAlchemy(app)
 class User(db.Model):
     """
     representation of the user table
-    the table has the following column: id, firstname, lastname, email, passwordhash
+    the table has the following column: id, firstname, lastname, email, username, passwordhash
     """
     __tablename__ = 'user1'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     firstname = db.Column(db.String(50), nullable=False)
     lastname = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
     passwordhash = db.Column(db.String(50), nullable=False)
 
 with app.app_context():
