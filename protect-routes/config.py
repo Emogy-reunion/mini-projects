d4aa14affde3fb1f7b9a7ae7c201d5d9b730f22d
Emogy-reunion <mv7786986@gmail.com>
2024-06-30 19:13:59 +0300

    doc: commented code

--- protect-routes/config.py
+++ protect-routes/config.py
@@ -1,4 +1,7 @@
 class Config:
+    """
+    definition of configuration files
+    """
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://mark:7hhYhn>4@localhost/flaskpractice'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'MY-SECRET-KEY'
