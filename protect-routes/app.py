e7fafca8897318e9ad08c589f88b986a684f2904
Emogy-reunion <mv7786986@gmail.com>
2024-07-03 05:17:34 +0300

    add: add the register route

--- protect-routes/app.py
+++ protect-routes/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for, redirect, flash, render_template
 from config import Config
 from model import db, User
 from form import RegistrationForm
@@ -12,6 +12,56 @@ app.config.from_object(Config)
 #initialize the db instance with the app instance
 db.init_app(app)
 
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """
+    adds users to the database
+    """
+    form = RegistrationForm()
+    
+    if form.validate_on_submit():
+        """
+        checks if the form contains valid data
+        if the data is valid it is extracted
+        the data is then added to the database
+        """
+        firstname = form.firstname.data
+        middlename = form.middlename.data
+        lastname = form.lastname.data
+        email = form.email.data.lower()
+        username = form.username.data.lower()
+        password = form.password.data
+
+        #check if the username is used
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash("Username is already taken. Please choose a different one.", "danger")
+            return redirect(url_for('register'))
+
+        #check if email is used
+        user1 = User.query.filter_by(email=email).first()
+        if user1:
+            flash("Email is already in use. Please use a different one.", "danger")
+            return redirect(url_for('register'))
+
+       #if email and username aren't taken create the account
+       try:
+            new_user = User(firstname=firstname, middlename=middlename, lastname=lastname,
+                            email=email, username=username, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash("Registration successful!", "success")
+            retun redirect(url_for('login'))
+        except Exception as e:
+            db.session.rollback()
+            flash("An error occurred during account creation. Please try again.", "danger")
+            return redirect(url_for('register'))
+    return render_template('register.html', form=form)
+
+
+
+        
 
 
 if __name__ == '__main__':
