742e2a1a0ded84c418e01ea55f390b379e1f031a
Emogy-reunion <mv7786986@gmail.com>
2024-07-05 07:53:48 +0300

    fix: remove bcrypt instantiation

--- protect-routes/app.py
+++ protect-routes/app.py
@@ -13,7 +13,6 @@ app.config.from_object(Config)
 
 #initialize the db instance with the app instance
 db.init_app(app)
-bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
