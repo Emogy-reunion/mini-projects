750fb583b8727b177bd382d89f9c19bbb47cf97c
Emogy-reunion <mv7786986@gmail.com>
2024-07-15 18:34:03 +0300

    create: first commit after creating the role_required decorator

---
+++ protect-routes/role_decorator.py
@@ -0,0 +1,22 @@
+from functools import wraps
+
+def role_required(role):
+    '''
+    - This function is a decorator that checks the current user's role
+    - It takes a single argument role - role required to access resource
+    - If the user's role doesn't match the required role, they are redirected to
+      unauthorized page
+    - It returns a decorator
+    '''
+
+    def wrapper(f):
+    
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+
+            # check user roles
+            if current_user.role != role:
+                return redirect(url_for('dash.unauthorized'))
+            return f(*args, **kwargs)
+        return decorated_function
+    return wrapper
