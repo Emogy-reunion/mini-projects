119903d3e79cb9d7e3592c96d6ad1d13f92929a3
Emogy-reunion <mv7786986@gmail.com>
2024-07-15 04:47:39 +0300

    fix: import flash

--- protect-routes/routes/auth.py
+++ protect-routes/routes/auth.py
@@ -1,4 +1,4 @@
-from flask import url_for, Blueprint, redirect, request, render_template
+from flask import url_for, Blueprint, redirect, request, render_template, flash
 from form import RegistrationForm, LoginForm
 from model import User, db
 from  flask_login import login_user
