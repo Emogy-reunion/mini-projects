dd5c21667fd48b4df8006459f5ece1ef42db8053
Emogy-reunion <mv7786986@gmail.com>
2024-08-11 18:36:27 +0300

    document: write module documentation

--- upload-file/routes/update.py
+++ upload-file/routes/update.py
@@ -1,3 +1,7 @@
+'''
+This module handles updating posts and user profile
+edit blueprint is registered and used to create routes
+'''
 from flask import Blueprint
 from flask_login import login_required
 
