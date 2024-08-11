cf61dbf57a6c3f887013a15126f94fa90e344736
Emogy-reunion <mv7786986@gmail.com>
2024-08-11 18:33:39 +0300

    document: write module documentation

--- upload-file/routes/dashboard.py
+++ upload-file/routes/dashboard.py
@@ -1,3 +1,8 @@
+'''
+This module handles rendering user dashboards
+dash blueprint is registered
+The blueprint is used to create routes
+'''
 from flask import Blueprint, render_template
 from flask_login import login_required
 
