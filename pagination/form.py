c534234cf8f622fe015593ad91f6808ce14c21e3
Emogy-reunion <mv7786986@gmail.com>
2024-08-21 06:04:01 +0300

    create the registration form class to represent the registration form fields

--- pagination/form.py
+++ pagination/form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, InputField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
+from wtforms.validators import DataRequired, InputRequired, EqualTo
 
 class LoginForm(FlaskForm):
     '''
@@ -11,3 +11,27 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[InputRequired()])
     remember = BooleanField('Remember me')
     submit = SubmitField('SIGN IN')
+
+
+class RegistrationForm(FlaskForm):
+    '''
+    represents the registration form fields
+    '''
+
+    firstname = StringField('First Name', validators=[InputRequired()])
+    lastname = StringField('Last Name', validators=[InputRequired()])
+    gender = SelectField('Gender', choices=[('M', 'Male'), ('F', 'Female')], validators=[InputRequired()])
+    email = StringField('Email', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[
+        InputRequired(),
+        Length(min=8, message='Password must be at least 8 characters long!'),
+        Regexp(r'(?=.*[A-Z])', message="Password must contain at least one uppercase letter!"),
+        Regexp(r'(?=.*[a-z])', message="Password must contain at least one lowercase letter!"),
+        Regexp(r'(?=.*\W)', message="Password must contain at least one special character!")
+        ])
+    confirmpassword = PasswordField('Confirm Password', validators=[
+        InputRequired(),
+        EqualTo('password', message='Passwords must match!')
+        ])
+    submit = SubmitField('SIGN UP')
+
