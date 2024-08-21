4397f7f75e03deac47be423953390f6868c937ea
Emogy-reunion <mv7786986@gmail.com>
2024-08-21 06:07:06 +0300

    register the db object with the app and create the tables

--- pagination/app.py
+++ pagination/app.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from config import Config
+from model import db, User
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
