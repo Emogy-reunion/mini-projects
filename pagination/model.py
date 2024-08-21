e5c91d8476f53b7b3dc5047eb69ab41f7573dad4
Emogy-reunion <mv7786986@gmail.com>
2024-08-21 05:34:39 +0300

    create: create the user model class to represent table columns

---
+++ pagination/model.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    '''
+    This class represents the columns of the user table
+    '''
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    firstname = db.Column(db.String(50), nullable=False)
+    lastname = db.Column(db.String(50), nullable=False)
+    gender = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
