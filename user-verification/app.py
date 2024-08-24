756ac54f1ea85093a25757baab12e2abb74e0b3c
Emogy-reunion <mv7786986@gmail.com>
2024-08-24 12:45:40 +0300

    initialize bcrypt and mail instances with the app, register the auth blueprint

--- user-verification/app.py
+++ user-verification/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from model import db, User
+from model import db, bcrypt, User
+from routes.authentication import mail, auth
 from config import Config
 
 app = Flask(__name__)
@@ -7,6 +8,10 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
+bcrypt.init_app(app)
+mail.init_app(app)
+
+app.register_blueprint(auth)
 
 with app.app_context():
     db.create_all()
