b82a6586ab9c228ade9a2092f5f7a03f10e7a17c
Emogy-reunion <mv7786986@gmail.com>
2024-08-24 10:29:09 +0300

    fix: add quotation marks to the getenv arguments

--- user-verification/config.py
+++ user-verification/config.py
@@ -4,6 +4,6 @@ import os
 load_dotenv()
 
 class Config():
-    SECRET_KEY = os.getenv(SECRET_KEY)
-    SQALCHEMY_DATABASE_URI = os.getenv(DATABASE_URI)
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
