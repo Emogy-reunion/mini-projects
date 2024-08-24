5322c97bc4dc6b2d24d4ec0fbfaf913d78b4ffbc
Emogy-reunion <mv7786986@gmail.com>
2024-08-24 10:22:57 +0300

    add the app's configuration settings

---
+++ user-verification/config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Config():
+    SECRET_KEY = os.getenv(SECRET_KEY)
+    SQALCHEMY_DATABASE_URI = os.getenv(DATABASE_URI)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
