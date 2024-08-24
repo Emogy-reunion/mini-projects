83c3589cdc61752d4601024c2ab2704999b85754
Emogy-reunion <mv7786986@gmail.com>
2024-08-24 10:15:50 +0300

    initialize the sqlalchemy instance and create the tables

--- user-verification/app.py
+++ user-verification/app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from model import db, User
 
 app = Flask(__name__)
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 if __name__ = '__main__':
     app.run(debug=True)
