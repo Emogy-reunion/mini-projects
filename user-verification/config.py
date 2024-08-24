0d447c6be57ddf72ab54c5993d0cc292e358b743
Emogy-reunion <mv7786986@gmail.com>
2024-08-24 10:33:32 +0300

    fix: fix the typo errors

--- user-verification/config.py
+++ user-verification/config.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 class Config():
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
