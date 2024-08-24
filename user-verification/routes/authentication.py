cb812d1e5ec9ff5a09fa08a401f9c8925b512133
Emogy-reunion <mv7786986@gmail.com>
2024-08-24 12:56:03 +0300

    create a function that sends the verification token via email

--- user-verification/routes/authentication.py
+++ user-verification/routes/authentication.py
@@ -1,8 +1,11 @@
 from flask import Flask, Blueprint, render_template, request
 from form import RegistrationForm
 from model import db, User
+from flask_mail import Mail, Message
 
 auth = Blueprint('auth', __name__)
+
+mail = Mail()
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
@@ -37,5 +40,17 @@ def register():
                     return jsonify({'error': 'An unexpected error occurs!'})
         else:
             return jsonify({'errors': form.errors})
+
+def send_verification_email(user):
+    token = user.generate_token()
+
+    verification_url = url_for('verify_email', token, _external=True)
+    msg = Message(
+            subject='Verify your email',
+            sender='info.markrealestateapp734@gmail.com',
+            recipients=[user.email]
+            )
+    msg.body(f'Click the following link to verify your email address:{verification_url}')
+    mail.send(msg)
 
 
