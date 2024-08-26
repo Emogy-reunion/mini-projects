a41e0bfbaa7830695aabf670075bee2a46cfdba9
Emogy-reunion <mv7786986@gmail.com>
2024-08-26 05:09:57 +0300

    register the dash blueprint to initialize it with the app

--- user-verification/app.py
+++ user-verification/app.py
@@ -1,6 +1,7 @@
 from create_app import create_app
 from model import db, bcrypt, User
 from routes.authentication import mail, auth
+from routes.dashboard import dash
 
 
 app = create_app()
@@ -11,6 +12,7 @@ bcrypt.init_app(app)
 mail.init_app(app)
 
 app.register_blueprint(auth)
+app.register_blueprint(dash)
 
 with app.app_context():
     db.create_all()
