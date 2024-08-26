d114847f31266003e18d1d2b0eea145e9468d58a
Emogy-reunion <mv7786986@gmail.com>
2024-08-26 05:14:15 +0300

    initialize LoginManager which handles session management

--- user-verification/app.py
+++ user-verification/app.py
@@ -2,11 +2,13 @@ from create_app import create_app
 from model import db, bcrypt, User
 from routes.authentication import mail, auth
 from routes.dashboard import dash
+from flask_login import LoginManager
 
 
 app = create_app()
 
-
+loginmanager = LoginManager(app)
+loginmanager.login_view = 'login'
 db.init_app(app)
 bcrypt.init_app(app)
 mail.init_app(app)
@@ -16,6 +18,10 @@ app.register_blueprint(dash)
 
 with app.app_context():
     db.create_all()
+
+@loginmanager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 if __name__ == '__main__':
     app.run(debug=True)
