079f29c4721125b7c7502c7a6f677b7ae380c1cc
Emogy-reunion <mv7786986@gmail.com>
2024-09-01 21:14:10 +0300

    handle function resetting

--- user-verification/routes/authentication.py
+++ user-verification/routes/authentication.py
@@ -1,5 +1,5 @@
 from flask import Flask, Blueprint, render_template, request, url_for, redirect, flash, jsonify
-from form import RegistrationForm, LoginForm, ReverificationForm
+from form import RegistrationForm, LoginForm, ReverificationForm, ResetForm, ForgotPassword
 from model import db, User
 from flask_mail import Mail, Message
 from flask_login import login_user
@@ -130,3 +130,71 @@ def resend_verification():
                 flash('Incorrect email. Try again!')
                 return redirect(request.url)
     return render_template('reverification.html', form=form)
+
+
+@auth.route('forgot_password', methods=['GET', 'POST'])
+def forgot_password():
+
+    form = ForgotPassword()
+
+    if request.method == 'GET':
+        return render_template('forgot_password.html', form=form)
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            email = form.email.data
+
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            reset_email(user)
+            flash('A password reset link has been sent. Check your email!', 'success')
+            return redirect(request.url)
+        else:
+            flash('Account not found! Please try again!', 'error')
+            return redirect(request.url)
+
+def reset_email(user):
+    token = user.generate_token()
+
+    verification_url = url_for('auth.reset_email', token=token, _external=True)
+    msg = Message(
+            Subject='Password reset',
+            sender='info.markrealestateapp734@gmail.com'
+            recipients=[user.email])
+    msg.body = f'A password reset request has been made for your account, if you made the request click the following link to reset password {verification_url}. If you did not make the request ignore this email'
+    mail.send(msg)
+
+@auth.route('/reset_email/<token>')
+def reset_email(token):
+    user = User.verify_token(token)
+
+    if user:
+        return redirect(url_for('auth.reset_password'))
+    else:
+        flash('The verification link has expired or is invalid. Try again!', 'error')
+        return redirect(url_for('auth.forgot_password'))
+
+@auth.route('/reset_password', methods=['GET', 'POST'])
+def reset_password():
+
+    form = ResetForm()
+
+    if request.method == 'GET':
+        return render_template('reset.html', form=form)
+    else:
+        form = ResetForm(request.form)
+        if form.validate_on_submit():
+            password = form.password.data
+
+            try:
+                user = User.query.filter_by(id=current_user.id).first()
+                user.generate_password_hash(password)
+                db.session.commit()
+                return jsonify({'success': 'Password successfully changed'})
+            except Exception as e:
+                db.session.rollback()
+                return jsonify({'error': 'An error occurred. Try again!'})
+        else:
+            return jsonify({"errors": "form.errors"})
+            
